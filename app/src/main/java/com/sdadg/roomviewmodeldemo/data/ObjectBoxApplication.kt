4b1aa1ff450215795f34f104062b99a146fbdfb6
Oscar Zhang <oscarz@esub.com>
2018-07-16 14:15:16 -0700

    Added singleton boxstore var

--- app/src/main/java/com/sdadg/roomviewmodeldemo/data/ObjectBoxApplication.kt
+++ app/src/main/java/com/sdadg/roomviewmodeldemo/data/ObjectBoxApplication.kt
@@ -1,13 +1,16 @@
 package com.sdadg.roomviewmodeldemo.data
 
 import android.app.Application
+import com.sdadg.roomviewmodeldemo.MyObjectBox
+import io.objectbox.BoxStore
 
 class ObjectBoxApplication : Application() {
 
     //Must build with object box entity before this generated class appears
-//    val boxStore
+    lateinit var boxStore: BoxStore
 
     override fun onCreate() {
         super.onCreate()
+        boxStore = MyObjectBox.builder().androidContext(this).build()
     }
 }
