1ca1761a7cc0fa4f853c443cdf1df605bb2062f4
Oscar Zhang <oscarz@esub.com>
2018-07-16 14:11:02 -0700

    Applied plugin of objectbox to gradle

---
+++ app/src/main/java/com/sdadg/roomviewmodeldemo/data/ObjectBoxApplication.kt
@@ -0,0 +1,13 @@
+package com.sdadg.roomviewmodeldemo.data
+
+import android.app.Application
+
+class ObjectBoxApplication : Application() {
+
+    //Must build with object box entity before this generated class appears
+//    val boxStore
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
